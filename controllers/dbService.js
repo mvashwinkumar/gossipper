bf7729a5f30c5ada5812c4d02ac2f8c3d51f5ba9
mvashwinkumar <mvashwinkumar@gmail.com>
2015-10-19 22:40:47 +0800

    Single mongodb instance for service implemented

--- controllers/dbService.js
+++ controllers/dbService.js
@@ -1,85 +1,77 @@
 var url = 'mongodb://testuser:testpassword@ds041164.mongolab.com:41164/gossipper';
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
+var db;
+var coll;
 
 var dbserve = {};
 console.log(url);
 dbserve.init = function() {
 	console.log('dbService module initialized');
+	MongoClient.connect(url, function(err,database) {
+		db = database;
+		coll = db.collection('stickynotes');
+	});
 };
 
 // create note
 dbserve.insertNote = function(note, callback) {
-	MongoClient.connect(url, function(err,db) {
-		db.collection('stickynotes').insertOne({
-			'id' : note.id,
-			'title' : note.title,
-			'body' : note.body,
-			'top' : note.top,
-			'left' : note.left
-		}, function(err,result) {
-			console.log("Inserted a document into the stickynotes collection.");
-			callback(result.ops[0]);
-			db.close();
-		});
+	coll.insertOne({
+		'id' : note.id,
+		'title' : note.title,
+		'body' : note.body,
+		'top' : note.top,
+		'left' : note.left
+	}, function(err,result) {
+		console.log("Inserted a document into the stickynotes collection.");
+		callback(result.ops[0]);
 	});
+	
 };
 
 // read note
 dbserve.getNote = function(id, callback) {
-	MongoClient.connect(url, function(err, db) {
-	  var cursor =db.collection('stickynotes').find({ "id": id });
-	   cursor.each(function(err, result) {
-	      callback(result);
-	      db.close();
-	   });
-	});
+  var cursor =coll.find({ "id": id });
+   cursor.each(function(err, result) {
+      callback(result);
+   });
+	
 };
 
 dbserve.getAllNotes = function(callback) {
-	console.log('dbserve.getAllNotes called');
-	MongoClient.connect(url, function(err, db) {
-	  db.collection('stickynotes').find().toArray().then(
+	coll.find().toArray().then(
 	  	function(notes) {
 	  		console.dir(notes);
 	  		callback(notes);
-	  		db.close();
-	  	});
-	});
-}
-
-// update note
-dbserve.updateNote = function(note, callback) {
-	MongoClient.connect(url, function(err, db) {
-		db.collection('stickynotes').findAndModify(
-			{id:note.id}, 
-			[['id', 1]], 
-			{
-				$set : {
-					title: note.title,
-					body: note.body,
-					top: note.top,
-					left: note.left
-				}
-			}, 
-			{new : true}, 
-			function (err, documents) {
-	        	console.log(documents);
-	        	callback(documents.value);
-	        	db.close();
-	    });		
 	});
 };
 
+// update note
+dbserve.updateNote = function(note, callback) {
+	coll.findAndModify(
+		{id:note.id}, 
+		[['id', 1]], 
+		{
+			$set : {
+				title: note.title,
+				body: note.body,
+				top: note.top,
+				left: note.left
+			}
+		}, 
+		{new : true}, 
+		function (err, documents) {
+        	console.log(documents);
+        	callback(documents.value);
+    });		
+};
+
 // delete note
-dbserve.deleteNote = function(id, callback) {
-	MongoClient.connect(url, function(err, db) {
-		db.collection('stickynotes').deleteOne({ "id": id },
-	      function(err, result) {
-	         callback(result);
-	         db.close();
-	      });
-	});
+dbserve.deleteNote = function(id, callback) {	
+	coll.deleteOne({ "id": id },
+      function(err, result) {
+         callback(result);
+    });	
 };
 
 module.exports = dbserve;
